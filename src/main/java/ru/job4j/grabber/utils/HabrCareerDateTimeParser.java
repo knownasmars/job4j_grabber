ee97161328885e0a09423d122126cc0bcc534664
Mars <knownasmars@gmail.com>
2023-01-20 13:10:34 +0300

    2.1. Преобразование даты [#289476 #382848]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -1,14 +1,13 @@
 package ru.job4j.grabber.utils;
 
 import java.time.*;
-import java.time.format.DateTimeFormatter;
+
+import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
-    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_DATE_TIME;
 
     @Override
     public LocalDateTime parse(String parse) {
-        LocalDateTime localDateTime = LocalDateTime.parse(parse, dateTimeFormatter);
-        return localDateTime;
+        return LocalDateTime.parse(parse, ISO_DATE_TIME);
     }
 }
