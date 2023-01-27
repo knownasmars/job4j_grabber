ed6635ced1cf955ccf5d21319f658e07f49da65e
Mars <knownasmars@gmail.com>
2023-01-27 13:14:14 +0300

    2. SQL Select by company [#2760 #385444]

--- db/2. SQL Select by company [#2760]/exam_sql_task_db/select_companies_max_employees.sql
+++ db/2. SQL Select by company [#2760]/exam_sql_task_db/select_companies_max_employees.sql
@@ -3,10 +3,10 @@ from person p
 left join company c 
 on p.company_id = c.id
 group by c.name
-having c.count = (select max(t.count) from 
-					(select c.name, count(*)
-						from person p 
-						left join company c 
-						on p.company_id = c.id
-						group by c.name 
-						having count(p.name) = 3) as t);
+having count(p.company_id) 
+	= (select count(*) as cid
+		from person p
+		group by p.company_id
+		order by cid desc
+		limit 1);
+		
