89b5e4304a3acc41d414def279a397de9387c3c1
Mars <knownasmars@gmail.com>
2023-02-16 15:07:45 +0300

    4. Шаблонизатор. [#855 #392286]

--- src/main/java/ru/job4j/template/TemplateGenerator.java
+++ src/main/java/ru/job4j/template/TemplateGenerator.java
@@ -2,7 +2,7 @@ package ru.job4j.template;
 
 import java.util.Map;
 
-public class TemplateGenerator implements Generator{
+public class TemplateGenerator implements Generator {
     @Override
     public String produce(String template, Map<String, String> args) {
         return null;
